5e17e7ddee04475a5f2b84a1065e0b2d578a991e
Zeyu <zyliu16@student.monash.edu>
2020-10-20 21:11:56 +0800

    add documentation for package

---
+++ R/key_crop_yield.R
@@ -0,0 +1,9 @@
+#' Global key crop yield
+#'
+#' This data set contains crop yields from 249 countries
+#'
+#' TODO: Add description of the columns
+#'
+#' @source
+#' The data below comes from Our World in Data and source from Tidy Tuesday.
+"key_crop_yields"
