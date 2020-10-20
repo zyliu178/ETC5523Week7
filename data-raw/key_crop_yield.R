dd34cd240ed7907789c89a69324e9fa7c07b2705
Zeyu <zyliu16@student.monash.edu>
2020-10-20 20:12:40 +0800

    miss one file

--- data-raw/key_crop_yield.R
+++ data-raw/key_crop_yield.R
@@ -1,3 +1,17 @@
 ## code to prepare `key_crop_yield` dataset goes here
 
-usethis::use_data(key_crop_yield, overwrite = TRUE)
+library(readr)
+library(janitor)
+library(tidyr)
+library(magrittr)
+library(dplyr)
+url <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv'
+key_crop_yields <- read_csv(url) %>% clean_names() %>%
+  pivot_longer(
+    cols = c(entity, -code, -year),
+    names_to = "crop",
+    values_to = "tonnes_per_hectare", names_pattern = "([^_]+)"
+  ) %>%
+  rename(country = entity)
+
+usethis::use_data(key_crop_yields, overwrite = TRUE)
